8a07411a6504d3d066553a9a83d3083c9dbcfc18
MeasureO <42300157+MeasureO@users.noreply.github.com>
2023-10-26 09:57:18 +0300

    Create Karatsuba.py

---
+++ Karatsuba.py
@@ -0,0 +1,17 @@
+# n - степень двойки
+def Karatsuba(x, y, n):
+    if n == 1:
+        return x * y
+    else:
+        a, b = x // 10 ** (n // 2), x % 10 ** (n // 2)
+        c, d = y // 10 ** (n // 2), y % 10 ** (n // 2)
+        p = a + b
+        q = c + d
+        ab = Karatsuba(a, b, n)
+        cd = Karatsuba(c, d, n)
+        pq = Karatsuba(p, q, n)
+        return 10 ** n * Karatsuba(a, c, n // 2) + 10 ** (n // 2) * (pq - ab - cd) + Karatsuba(b, d, n // 2)
+
+x, y, n = map(int, input().split())
+print(f"Recursive multiplication: {Karatsuba(x, y, n)}")
+print(f"Multiplication: {x * y}")
